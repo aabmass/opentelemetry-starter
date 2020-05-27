e59c0cd447c156c1336d38afcf2048f4976dc32b
Aaron Abbott <aaronabbott@google.com>
2020-05-27 15:33:33 +0000

    Clean up deploy script, allow passing branch

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+## Optionally, pass BRANCH envar (defaults to master)
+
 for host in finder supplier vendor
 do
     gcloud beta compute ssh --zone "us-central1-a" "$host" --project "otel-starter-project" -- << ENDSSH
-    sudo -u foodfinder -HE sh -c 'cd /home/foodfinder/opentelemetry-starter && git checkout opencensus && git pull'
+    sudo -u foodfinder -HE bash << ENDSUDO
+        cd /home/foodfinder/opentelemetry-starter
+        git checkout ${BRANCH:-master}
+        git pull
+ENDSUDO
     sudo systemctl restart $host.service
 ENDSSH
 done
